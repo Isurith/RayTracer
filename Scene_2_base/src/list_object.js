4fadd9220fdd47a1bd15ad599e4d397c78288e44
boiven_d <boiven_d@epitech.eu>
2015-07-16 10:55:25 +0200

    RayTracer

---
+++ Scene_2_base/src/list_object.js
@@ -0,0 +1,26 @@
+/*
+** Nouvel objet :
+**
+** tab[x] = fonction_objet(core,
+** [tableau de couleur + transparence(0-100) + reflexion(0-2) + brillance(0-2+)]
+** [tableau de position + numéro de l'objet + angle ou rayon]
+** [tableau de rotation + limite(haute/basse)]);
+**
+** fonctions : sphere (rayon) / plan (0) / cylindre (rayon + limit H & B) / cone (angle + limit H & B) / ellyps(rayon)
+** core : structure générale
+*/
+
+function list_object2(core, tab, c)
+{
+    return (tab);
+}
+
+function list_object(core, tab, c)
+{
+    tab[c] = sphere(core, [50, 50, 50, 100, 1, 1], [0, 0, 0, c, 200], [0, 0, 0, null]);
+    tab[++c] = plan(core, [150, 50, 255, 100, 1, 1], [0, -100, 0, c, 50], [90, 0, 0, null]);
+    tab[++c] = cylindre(core, [100, 255, 50, 100, 1, 1], [0, 0, -400, c, 100], [90, 0, 0, null, null]);
+    tab[++c] = cone(core, [255, 150, 50, 100, 1, 1], [0, 300, 400, c, 10], [90, 0, 0, 0, -500]);
+    tab = list_object2(core, tab, c);
+    return (tab);
+}
